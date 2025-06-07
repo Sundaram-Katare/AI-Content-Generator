421e29e5da4a988df83d9c2f5d923c74083cfe19
Sundaram-Katare <sundaramkatare53@gmail.com>
2025-06-07 12:25:11 +0530

    chnge in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: {
+      'localhost': true,
+      'https://ai-content-generator-server-b923.onrender.com': true,
   }
+  },
 });
